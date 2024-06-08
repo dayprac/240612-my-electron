841a7b1e1666194b9aeaa9a826a1a8313c0c2fc9
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-06-09 06:38:04 +0800

    可以加载jable网页

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-// ffmpeg
-const { exec } = require("node:child_process");
-const ls = exec(
-  "ffmpeg -f lavfi -i mandelbrot -f mp4 \
--movflags frag_keyframe -preset ultrafast -pix_fmt yuv420p -listen 1 http://localhost:8000"
-);
-
-console.log(ls.spawnargs);
-
-// html
-const http = require("http");
-const html = `
-    <video controls autoplay src=http://localhost:8000 />
-`;
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(html);
-  })
-  .listen(8604);
-
 // window
 const { app, BrowserWindow } = require("electron");
 app.whenReady().then(() => {
   const window = new BrowserWindow();
-  window.loadURL("http://localhost:8604/");
+  window.loadURL("https://fs1.app/videos/ssis-313/");
 });
