93fc936a23f8c393928316deacffa68ccb83e849
Qian Zhiqiang <qianzq@touzhiwang.com>
2024-06-12 16:15:49 +0800

    feat: electron-forge打包

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const path = require("path");
 app.whenReady().then(async () => {
   const window = new BrowserWindow();
   // console.log(app.getPath("userData")); // /Users/qianzhiqiang/Library/Application Support/my-electron
-  await session.defaultSession.loadExtension(
-    path.join(__dirname, "/minimal-chrome-extension"),
-    // allowFileAccess is required to load the devtools extension on file:// URLs.
-    { allowFileAccess: true }
-  );
+  // build版本中报错，导致白屏，开发者工具也打不开。先注释掉。
+  // await session.defaultSession.loadExtension(
+  //   path.join(__dirname, "/minimal-chrome-extension"),
+  //   // allowFileAccess is required to load the devtools extension on file:// URLs.
+  //   { allowFileAccess: true }
+  // );
   window.loadFile("./minimal-chrome-extension/demo.html");
 });
